5e2c9e09e2a52f07c2c380e4ef0b0363b85855be
Mohamad Al-khatib <alkhatib.m804@gmail.com>
2026-01-30 17:58:31 +0100

    husky

--- lib/services/index.ts
+++ lib/services/index.ts
@@ -11,23 +11,57 @@ export { projectService } from './project.service';
 // Server-side services (for use in API routes and server components)
 export {
   generateBlogArticle,
-  generateObjectFromAI, type GenerateBlogArticleInput,
-  type GenerateObjectOptions, type GeneratedArticle
+  generateObjectFromAI,
+  type GenerateBlogArticleInput,
+  type GenerateObjectOptions,
+  type GeneratedArticle,
 } from './ai.service.server';
-export { blogSchema, blogServiceServer, blogUpdateSchema } from './blog.service.server';
+export {
+  blogSchema,
+  blogServiceServer,
+  blogUpdateSchema,
+} from './blog.service.server';
 export { clientServiceServer } from './client.service.server';
 export {
-  createLanguage, deleteLanguage, getAllLanguages,
-  getLanguageById, languageSchema,
-  languageUpdateSchema, updateLanguage
+  createLanguage,
+  deleteLanguage,
+  getAllLanguages,
+  getLanguageById,
+  languageSchema,
+  languageUpdateSchema,
+  updateLanguage,
 } from './language.service.server';
-export { projectSchema, projectServiceServer, projectUpdateSchema } from './project.service.server';
+export {
+  projectSchema,
+  projectServiceServer,
+  projectUpdateSchema,
+} from './project.service.server';
 
 // Re-export types from entities (for convenience)
 export type {
-  Blog, BlogListParams, BlogListResponse,
-  BlogResponse, Client, ClientListParams, ClientListResponse,
-  ClientResponse, CreateBlogDto, CreateClientDto, CreateLanguageDto, CreateProjectDto, Language, LanguageListParams, LanguageListResponse,
-  LanguageResponse, Project, ProjectListParams, ProjectListResponse,
-  ProjectResponse, Status, UpdateBlogDto, UpdateClientDto, UpdateLanguageDto, UpdateProjectDto
+  Blog,
+  BlogListParams,
+  BlogListResponse,
+  BlogResponse,
+  Client,
+  ClientListParams,
+  ClientListResponse,
+  ClientResponse,
+  CreateBlogDto,
+  CreateClientDto,
+  CreateLanguageDto,
+  CreateProjectDto,
+  Language,
+  LanguageListParams,
+  LanguageListResponse,
+  LanguageResponse,
+  Project,
+  ProjectListParams,
+  ProjectListResponse,
+  ProjectResponse,
+  Status,
+  UpdateBlogDto,
+  UpdateClientDto,
+  UpdateLanguageDto,
+  UpdateProjectDto,
 } from '@/types/entities';
