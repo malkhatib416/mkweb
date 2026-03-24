9383efe22d2d6df0053624a51e79deece98dad8c
Mohamad Al-khatib <alkhatib.m804@gmail.com>
2026-03-24 14:44:33 +0100

    fix: command

--- utils/send-mail.ts
+++ utils/send-mail.ts
@@ -5,9 +5,12 @@ const SMTP_SERVER_HOST = process.env.SMTP_SERVER_HOST;
 const SMTP_SERVER_USERNAME = process.env.SMTP_SERVER_USERNAME;
 const SMTP_SERVER_PASSWORD = process.env.SMTP_SERVER_PASSWORD;
 
+const smtpPort = Number(process.env.SMTP_SERVER_PORT || 587);
+
 const transporter = nodemailer.createTransport({
   host: SMTP_SERVER_HOST,
-  secure: true,
+  port: smtpPort,
+  secure: smtpPort === 465,
   auth: {
     user: SMTP_SERVER_USERNAME,
     pass: SMTP_SERVER_PASSWORD,
